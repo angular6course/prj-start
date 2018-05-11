c4b7d35367cc2caf380c86596ed9ad9cba947dd4
nydi <stefan.nydegger@gmail.com>
2018-05-11 18:02:53 +0200

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {selector} from 'rxjs-compat/operator/publish';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html'
+})
+export class HeaderComponent {
+
+}
