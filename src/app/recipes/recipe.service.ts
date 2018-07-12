07e8bf8b39a9b777216fb3900cbe3dad3673f6c5
nydi <stefan.nydegger@gmail.com>
2018-07-12 21:15:47 +0200

    lecture 111

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,25 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-      'just a test description',
-      'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
-    new Recipe('Another Test Recipe',
-      'The description of the recipe',
-      'https://www.sensibus.com/deli/sites/sensibus.com/files/recipes/pasta-dish-2_0.jpg')
-
+    new Recipe('Tasty Schnitzel',
+      'a super tasty schnitzel',
+      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://static1.squarespace.com/static/55a985b9e4b0f561249e6e6f/t/56ef9239e707eb3a53f8f23d/1492450845240/XXXL+Fatburger%2C+Skinny+fries+and+a+Maui+milkshake.?format=750w',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes(): Recipe[] {
