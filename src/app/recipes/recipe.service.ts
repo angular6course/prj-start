c4c56cc3099481925a343fc6e42d040671fdb556
nydi <stefan.nydegger@gmail.com>
2018-08-16 22:58:42 +0200

    lecture 252

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -29,6 +29,12 @@ export class RecipeService {
   constructor(private readonly shoppingListService: ShoppingListService) {
   }
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+
+  }
+
   getRecipes(): Recipe[] {
     return this.recipes.slice();
   }
