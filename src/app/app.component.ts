a22a3ae5307366f2d7cec7f044d4c8d2035987c8
nydi <stefan.nydegger@gmail.com>
2018-08-17 19:29:32 +0200

    lecture 258

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCt416m7hSNkttR7kYB3SlyulX64qRA-4Q',
+      authDomain: 'ng-recipe-book-19bf9.firebaseapp.com'
+    });
   }
 }
