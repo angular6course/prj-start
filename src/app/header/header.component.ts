b7eb1b8a734dec0b804ab96a3fe38130d90ee627
nydi <stefan.nydegger@gmail.com>
2018-08-18 18:22:49 +0200

    lecture 264

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {DataStorageService} from '../shared/data-storage.service';
 import {Response} from '@angular/http';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,8 @@ import {Response} from '@angular/http';
 })
 export class HeaderComponent {
 
-  constructor(private readonly dataStorageService: DataStorageService) {
+  constructor(private readonly dataStorageService: DataStorageService,
+              public readonly authService: AuthService) {
   }
 
   onSaveData() {
