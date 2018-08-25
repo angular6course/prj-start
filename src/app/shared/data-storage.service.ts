65fa782df46dd3d3d1f9e4d460f7527b6b2d87d7
nydi <stefan.nydegger@gmail.com>
2018-08-25 13:29:40 +0200

    lecture 301

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map} from 'rxjs/operators/map';
@@ -16,7 +15,9 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
     return this.http.put('https://ng-recipe-book-19bf9.firebaseio.com/recipes.json?auth='
-      + token, this.recipeService.getRecipes());
+      + token, this.recipeService.getRecipes(), {
+      observe: 'body'
+    });
   }
 
   getRecipes() {
