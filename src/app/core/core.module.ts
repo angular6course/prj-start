80e9bf5e86c38f54e71ef467117ba51ca33e9749
nydi <stefan.nydegger@gmail.com>
2018-08-26 08:31:48 +0200

    lecture 306

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,8 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {RecipeService} from '../recipes/recipe.service';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
-import {AuthGuard} from '../auth/auth-guard.service';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from '../shared/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,11 @@ import {AuthGuard} from '../auth/auth-guard.service';
     AppRoutingModule,
     HeaderComponent
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService]
+  providers: [ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
 })
 export class CoreModule {
 
