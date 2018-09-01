84d473a9fb1c446d3889894a0869a04dc2434ff0
nydi <stefan.nydegger@gmail.com>
2018-09-01 10:09:27 +0200

    lecture 338

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -20,6 +20,6 @@ export class SignupComponent implements OnInit {
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.email;
-    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}))
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,13 +1,38 @@
 import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 import * as AuthActions from './auth.actions';
+import {map, mergeMap, switchMap} from 'rxjs/operators';
+import * as firebase from 'firebase';
+import {from} from 'rxjs';
 
 @Injectable()
 export class AuthEffects {
+
+  // @Effect({dispatch: false})  ohne dispatch: false gibt es einen neuen effect zurück,
+  // dann darf aber auch keine neue action am schluss (hier mergemap zurückgegeben werden)
   @Effect()
-  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP);
+  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP)
+    .pipe(map((action: AuthActions.TrySignup) => {
+        return action.payload;
+      }),
+      switchMap((authData: { username: string, password: string }) => {
+        return from(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+      }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        return [{
+          type: AuthActions.SIGNUP
+        },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          }
+        ];
+      })
+    );
 
   constructor(private actions$: Actions) {
-
   }
 }
