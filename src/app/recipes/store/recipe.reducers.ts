429862bb7cd0bf0037a952cdaddd7166aca7b617
nydi <stefan.nydegger@gmail.com>
2018-09-01 14:30:26 +0200

    lecture 347

---
+++ src/app/recipes/store/recipe.reducers.ts
@@ -0,0 +1,34 @@
+import {Recipe} from '../recipe.model';
+import {Ingredient} from '../../shared/ingredient.model';
+
+export interface FeatureState {
+  recipes: State;
+}
+
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: [
+    new Recipe('Tasty Schnitzel',
+      'a super tasty schnitzel',
+      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://static1.squarespace.com/static/55a985b9e4b0f561249e6e6f/t/56ef9239e707eb3a53f8f23d/1492450845240/XXXL+Fatburger%2C+Skinny+fries+and+a+Maui+milkshake.?format=750w',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ]
+}
+
+export function recipeReducer(state = initialState, action) {
+  return state;
+
+}
