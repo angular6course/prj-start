a9e9090de82a4558f8c2aad6b8a8264ecc04accf
nydi <stefan.nydegger@gmail.com>
2018-09-03 22:41:44 +0200

    lecture 354

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,39 +1,14 @@
 import {Actions, Effect} from '@ngrx/effects';
 import * as RecipeActions from '../store/recipe.actions';
-import {map, switchMap} from 'rxjs/operators';
+import {map, switchMap, withLatestFrom} from 'rxjs/operators';
 import {Recipe} from '../recipe.model';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import * as fromRecipe from '../store/recipe.reducers';
+import {Store} from '@ngrx/store';
 
 @Injectable()
 export class RecipeEffects {
-
-  constructor(private actions$: Actions, private readonly http: HttpClient) {
-  }
-
-  // @Effect()
-  // recipeFetch = this.actions$
-  //   .ofType(RecipeActions.FETCH_RECIPES)
-  //   .switchMap((action: RecipeActions.FetchRecipes) => {
-  //     return this.http.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
-  //       observe: 'body',
-  //       responseType: 'json'
-  //     });
-  //   })
-  //   .map(
-  //     (recipes) => {
-  //       console.log(recipes);
-  //       for (let recipe of recipes) {
-  //         if (!recipe['ingredients']) {
-  //           recipe['ingredients'] = [];
-  //         }
-  //       }
-  //       return {
-  //         type: RecipeActions.SET_RECIPES,
-  //         payload: recipes
-  //       };
-  //     }
-  //   );
 
   @Effect()
   recipeFetch = this.actions$
@@ -60,5 +35,21 @@ export class RecipeEffects {
           };
         }
       ));
+
+  @Effect({dispatch: false})
+  recipeStore = this.actions$
+    .ofType(RecipeActions.STORE_RECIPES)
+    .pipe(withLatestFrom(this.store.select('recipes')),
+      switchMap(([action, newState]) => {
+        const req = new HttpRequest('PUT', 'https://ng-recipe-book-19bf9.firebaseio.com/recipes.json',
+          // this.recipeService.getRecipes(), {reportProgress: true, params: new HttpParams().set('auth', token)});
+          newState.recipes, {reportProgress: true});
+        return this.http.request(req);
+      }));
+
+  constructor(private actions$: Actions,
+              private readonly http: HttpClient,
+              private store: Store<fromRecipe.FeatureState>) {
+  }
 }
 
